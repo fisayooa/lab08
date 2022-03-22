5a97d91c567acd0982b636275fd9bf7439c33fcd
fisayooa <90349504+fisayooa@users.noreply.github.com>
2022-03-21 20:25:38 -0400

    added usernameinput.py

--- usernameinput.py
+++ usernameinput.py
@@ -1,2 +1,26 @@
-userName = input ("Hello! Please enter your name!")
-print ("Hello", userName, "nice to meet you")
+Python 3.9.5 (v3.9.5:0a7dcbdb13, May  3 2021, 13:17:02) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: /Users/olufisayoayanfalu/Documents/helloWorld.py ==========
+Hello World
+>>> 
+=========== RESTART: /Users/olufisayoayanfalu/Documents/helloWorld.py ==========
+Hello World, my name is olufisayo and I love Python
+>>> 
+=========== RESTART: /Users/olufisayoayanfalu/Documents/helloWorld.py ==========
+Hello World, my name is olufisayo and I love Python
+Hello! Please enter your name! Olufisayo
+Hello  Olufisayo nice to meet you
+>>> 
+========= RESTART: /Users/olufisayoayanfalu/Documents/username input.py ========
+Hello! Please enter your name!Olufisayo
+Hello Olufisayo nice to meet you
+>>> 
+========= RESTART: /Users/olufisayoayanfalu/Documents/username input.py ========
+Hello! Please enter your name!fisayo
+Hello fisayo nice to meet you
+Hello! Please enter your facebook username! fisayo
+Hello! Please enter your facebook password! JphnDoe!
+Welcome to facebook!
+>>> 
